5a4f9fbfe032a59a3a4a6db37134aa66e18fa6a4
kumar1802 <prasanna.ameex@gmail.com>
2018-09-06 17:39:42 +0530

    first commit

---
+++ src/main/java/com/ameex/controller/GmailController.java
@@ -0,0 +1,24 @@
+package com.ameex.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class GmailController {
+	@RequestMapping("welcome")
+	public ModelAndView registratin() {
+		ModelAndView model=new ModelAndView();
+		model.addObject("Ok","welcome");
+		return model;
+	}
+	
+	@RequestMapping("hi")
+	public String hi() {
+		return "hi";
+	}
+
+}
